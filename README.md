41e59b48f2daa8bb214d32f174f35703c2ffbe7d
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-21 10:30:13 -0500

    Migración, crear directores

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ Tipos de Datos de Columnas
 
 Correr migración a SQLite3
 
-    $ sequel -m db/migrations -M 10 sqlite://db/coa.db
+    $ sequel -m db/migrations -M 11 sqlite://db/coa.db
 
 
 ---
---
+++ db/migrations/011_crear_directores.rb
@@ -0,0 +1,20 @@
+require 'sequel'
+
+Sequel.migration do
+  up do
+    create_table(:directores) do
+      primary_key :id
+    end
+
+    alter_table(:directores) do
+      add_foreign_key :sede_id, :sedes
+      add_foreign_key :doctor_id, :doctores
+    end
+	end
+
+  down do
+    drop_column directores, :sede_id
+    drop_column directores, :doctor_id
+    drop_table(:directores)
+	end
+end
